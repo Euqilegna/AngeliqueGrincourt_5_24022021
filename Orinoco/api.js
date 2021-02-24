f7b22f121cb6c1b4a8cbfb8997bd1b9e3a708ec4
Angelique Grincourt <angelique.grnt@gmail.com>
2021-02-24 10:14:16 +0100

    creation repo

---
+++ Orinoco/api.js
@@ -0,0 +1,19 @@
+class CameraApi {
+
+    apiUrl
+    constructor() {
+        this.apiUrl = "http://localhost:3000/api/cameras"
+    }
+
+    getAll() {
+
+
+        fetch(this.apiUrl, {
+            method: 'GET',
+        })
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
+}
