24644e11bfdf6d27707b398113dddb5c8283ddb7
angelique.grnt@gmail.com <angelique.grnt@gmail.com>
2021-03-16 16:35:31 +0100

    page panier

--- Orinoco/js/home.js
+++ Orinoco/js/home.js
@@ -2,12 +2,6 @@
 const api = new CameraApi()
 
 
-const redirectToProductPage = (idProduct) => {
-    localStorage.setItem("productId", idProduct);
-    // redirection vers la page produit
-    document.location.href = "productPage.html";
-}
-
 const loadData = async () => {
     const productList = await api.getAll()
     let containerOfProductList = ""
@@ -27,17 +21,30 @@ const loadData = async () => {
 }
 
 
+const redirectToProductPage = (idProduct) => {
+    localStorage.setItem("productId", idProduct);
+    // redirection vers la page produit
+    document.location.href = "productPage.html";
+}
+
+const redirectToShoppingCartPage = (idProduct) => {
+    localStorage.setItem('productId', idProduct);
+    document.location.href = "shoppingCartPage.html";
+} 
+
+
 const loadDataById = async () => {
     const camera = await api.getById(localStorage.getItem("productId"))
+    
     let containerOfProduct =
-        `<div class="cardContainer__card--product">
-    <div class="cardContainer__card--productLeftInfo">
-        <img id="img" src="${camera.imageUrl}" class="cardContainer__img--productImg"> </img>
-    </div>
+    `<div class="cardContainer__card--product">
+        <div class="cardContainer__card--productLeftInfo">
+            <img id="img" src="${camera.imageUrl}" class="cardContainer__img--productImg"> </img>
+        </div>  
     <div class="cardContainer__card--productRightInfo">
         <div class="cardContainer__description--productInfo">
             <span class="cardContainer__descriptionTitle--titlePageProduct"> ${camera.name} </span>
-            <span class="cardContainer__descriptionPrice--PricePageProduct">${camera.price} euros </span>
+            <span class="cardContainer__descriptionPrice--PricePageProduct"> ${camera.price} euros </span>
             <div class="cardContainer__personnalisationpOfProduct">
                 <span class="cardContainer__personnalisationpOfProduct--title"> Personnalisation :
                 </span>
@@ -55,13 +62,44 @@ const loadDataById = async () => {
             <div class="cardContainer__descriptionpOfProduct">
                 <span class="cardContainer__descriptionpOfProduct--title"> Descritpion : </span>
                 <div class="cardContainer__descriptionpOfProduct--text">
-                    <p> ${camera.description}</p>
+                    <p> ${camera.description} </p>
                 </div>
             </div>
+            <button class="cardContainer__buttonAddToShoppingCart" onclick="redirectToShoppingCartPage('${camera._id}')"> Ajouter au panier </button>
         </div>
     </div>
 </div>`
-
     document.getElementById("containerOfProduct").innerHTML = containerOfProduct
 }
+
+const loadProut = async () => {
+    const camera = await api.getById(localStorage.getItem("productId"))
+    let resumeOfProduct = 
+    `   <div class="containerCardShoppingCartPage">
+            <span class="containerCardShoppingCartPage__titleForm"> Résumé de la commande </span>
+        <div class="containerCardShoppingCartPage__rowInfo">
+            <img id="img" class="containerCardShoppingCartPage__img" src="${camera.imageUrl}" class=""> </img>
+                <span> ${camera.name} </span>
+                <span> ${camera.price} </span>
+        </div>
+
+        <select class="form-control selectNumberOfProduct">
+            <option> 1 </option>
+            <option> 2 </option>
+            <option> 3 </option>
+            <option> 4 </option>
+            <option> 5 </option>
+        </select>
+        <div class="containerCardShoppingCartPage__align">
+            <span>Total :</span>
+            <span> 6.80</span>
+        </div>
+        </div>
+        `
+           
+    document.getElementById("resumeOfProduct").innerHTML = resumeOfProduct
+}
+
+
+
 
